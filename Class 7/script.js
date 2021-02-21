d9c4993f501f6aac8fe0d47e9d36dd46fd00eec2
Vlatko Jakimovski <jakimovski_vlatko@yahoo.com>
2021-02-21 11:07:00 +0100

    Domasno Class 7

---
+++ Class 7/script.js
@@ -0,0 +1,74 @@
+let table = document.getElementById('tableResults');
+let nameSearch = document.getElementById('nameSearch');
+let emailSearch = document.getElementById('emailSearch');
+let phoneSearch = document.getElementById('phoneSearch');
+let idCounter = 0;
+
+function makeCall(url, callback) {
+    $.ajax({
+        url: url,
+        success: function(result) {
+            callback(result)
+
+            nameSearch.addEventListener("keydown", () => {
+                searchName(result);
+            });
+            emailSearch.addEventListener("keydown", () => {
+                searchEmail(result);
+            });
+            phoneSearch.addEventListener("keydown", () => {
+                searchPhone(result);
+            });
+
+        },
+        error: function(error) {
+            console.warn(error)
+        }
+    })
+}
+
+function writeTable (data){
+    table.innerHTML = ``;
+   data.map( el => table.innerHTML += `
+    <tr> 
+        <td>${el.name}</td>
+        <td>${el.email}</td>
+        <td>${el.phone}</td>
+    </tr>
+   ` )
+}
+
+function searchName(data) {
+    console.log(`Called searchName`);
+    let tempData = data.filter(el => el.name.toLowerCase().
+    includes(nameSearch.value.toLowerCase()));
+    writeTable(tempData);
+    console.log(idCounting(tempData));
+}
+function searchEmail(data) {
+    console.log(`Called searchName`);
+    let tempData = data.filter(el => el.email.toLowerCase().
+    includes(emailSearch.value.toLowerCase()))
+    writeTable(tempData);
+    console.log(idCounting(tempData));
+}
+function searchPhone(data) {
+    console.log(`Called searchName`);
+    let tempData = data.filter(el => el.phone.toLowerCase().
+    includes(phoneSearch.value.toLowerCase()))
+    writeTable(tempData);
+    console.log(idCounting(tempData));
+}
+
+function idCounting(data){
+    idCounter = 0;
+    data.forEach(el => {
+        idCounter += el.id;
+    });
+    return idCounter;
+}
+
+
+makeCall('https://jsonplaceholder.typicode.com/users',writeTable);
+// console.log(data);
+
